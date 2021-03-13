aad152ca40c5dfc1befa4071dc44a7792f326ba3
kito0 <drewshah0@gmail.com>
2021-03-12 22:00:38 -0700

    added basic client

---
+++ client/src/components/sidebar/SidebarOption.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../css/SidebarOption.css';
+
+export default function SidebarOption({ text, Icon }) {
+	return (
+		<div className="sidebarOption">
+			<h2>option</h2>
+		</div>
+	);
+}
